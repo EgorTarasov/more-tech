ed3057e85489518c93f8b6022f17aedba7cd723e
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2023-10-14 19:17:13 +0300

    cookie fix

--- internal/api/middleware/auth_middleware.go
+++ internal/api/middleware/auth_middleware.go
@@ -9,7 +9,7 @@ func AuthMiddleware(domain string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		_, err := c.Cookie("session")
 		if err != nil {
-			c.SetCookie("session", uuid.New().String(), 60*60*24*400, "/", domain, false, true)
+			c.SetCookie("session", uuid.New().String(), 60*60*24*400, "/", domain, false, false)
 		}
 
 		c.Next()
